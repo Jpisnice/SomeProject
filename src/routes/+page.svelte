fc65f92a7b0119d24cb61f00a581234db2e5f7bf
Jpisnice <www.janardhanpolle@gmail.com>
2025-06-09 12:41:48 +0530

    Initialized Project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Button from './../lib/components/ui/button/button.svelte';
+</script>
+<h1 class="scroll-m-20 text-balance text-4xl font-extrabold tracking-tight">
+    Welcome to Sveltekit
+  </h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button>
+    Click me Pls
+</Button>
