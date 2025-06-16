8f711e41747a38596ac017348440419fe4236013
Jpisnice <www.janardhanpolle@gmail.com>
2025-06-16 15:36:24 +0530

    replaced hardcoded base url with .env variable v2

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/svelte"
-import { env } from '$env/dynamic/private';
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: env.DATABASE_URL
+    baseURL: process.env.DATABASE_URL
 })
