51b438830383c9af4f7da76a5e9b5242cb88dc4e
Jpisnice <www.janardhanpolle@gmail.com>
2025-06-16 15:48:26 +0530

    added vercel url to env

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/svelte"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    // baseURL: process.env.DATABASE_URL
+    baseURL: process.env.DATABASE_URL
 })
--- src/lib/server/auth/index.ts
+++ src/lib/server/auth/index.ts
@@ -6,6 +6,7 @@ export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
     }),
+    trustedOrigins: [process.env.VERCEL_URL!],
     emailAndPassword: {  
         enabled: true
     },
